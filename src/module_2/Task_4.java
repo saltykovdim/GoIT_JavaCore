22a03579fb6c4095568adce67a2630e8240e5d1d
Dmitriy <saltykovdim@gmail.com>
2016-08-29 16:38:20 +0300

    задание 4+изменения4

--- src/module_2/Task_4.java
+++ src/module_2/Task_4.java
@@ -8,17 +8,21 @@ public class Task_4 {
 
         String ownerName = "Oww";
         double withdrawal = 100;
-       System.out.println(ownerName+" "+balanceAfterFunding(ownerNames,ownerName,balances,withdrawal));
+        System.out.println(ownerName+" "+balanceAfterFunding(ownerNames,ownerName,balances,withdrawal));
+        for (int i=0; i<balances.length; i++){
+            System.out.println(balances[i]);
+        }
     }
 
 
     public static double balanceAfterFunding (String array[], String name, int balances[], double withdrawal){
         int index=-1;
-        for (int i=0; i<array.length; i++){
+        for(int i=0; i<array.length; i++){
             if(name.equals(array[i])){
-                index=i;
+                balances[i]+=withdrawal;
+                index = i;
             }
         }
-        return balances[index]+withdrawal;
+        return balances[index];
     }
 }
