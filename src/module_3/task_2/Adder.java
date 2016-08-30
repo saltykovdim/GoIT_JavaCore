6e46532944130ed2738914afc51d7489585a9653
Dmitriy <saltykovdim@gmail.com>
2016-08-30 18:00:40 +0300

    модуль 3 зад2

---
+++ src/module_3/task_2/Adder.java
@@ -0,0 +1,12 @@
+package module_3.task_2;
+
+
+public class Adder extends Arithmetic{
+   public static boolean chek(int a, int b){
+       if (a >= b) {
+           return true;
+       }else{
+           return false;
+       }
+   }
+}
---
+++ src/module_3/task_2/Arithmetic.java
@@ -0,0 +1,8 @@
+package module_3.task_2;
+
+
+public class Arithmetic {
+    public static int add(int a, int b){
+        return a+b;
+    }
+}
