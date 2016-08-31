101fc415566e0792d57c4555dba46f3e71cbfec0
Dmitriy <saltykovdim@gmail.com>
2016-08-31 17:37:14 +0300

    модуль 3 зад2

--- src/module_3/task_2/Adder.java
+++ src/module_3/task_2/Adder.java
@@ -2,7 +2,7 @@ package module_3.task_2;
 
 
 public class Adder extends Arithmetic{
-   public static boolean chek(int a, int b){
+   public static boolean check(int a, int b){
        return (a >= b);
    }
 }
