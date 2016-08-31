a44624756e3a83f7d957670129f82a266d1f7085
Dmitriy <saltykovdim@gmail.com>
2016-08-31 10:29:21 +0300

    модуль 3 зад2+доработка

--- src/module_3/task_2/Adder.java
+++ src/module_3/task_2/Adder.java
@@ -3,10 +3,7 @@ package module_3.task_2;
 
 public class Adder extends Arithmetic{
    public static boolean chek(int a, int b){
-       if (a >= b) {
-           return true;
-       }else{
-           return false;
-       }
+       return (a >= b);
    }
 }
+
