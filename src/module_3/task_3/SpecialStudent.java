622e7164fac620d047646355330a609441f1d94d
Dmitriy <saltykovdim@gmail.com>
2016-08-31 15:47:37 +0300

    модуль 3 зад3

--- src/module_3/task_3/SpecialStudent.java
+++ src/module_3/task_3/SpecialStudent.java
@@ -5,4 +5,17 @@ public class SpecialStudent extends CollegeStudent{
     private long secretKey;
     private String email;
 
+    public SpecialStudent(String firstName, String lastName, int group, String collegeName) {
+        super(firstName, lastName, group, collegeName);
+    }
+
+    public SpecialStudent(String lastName, Course[] coursesTaken, int rating, String email) {
+        super(lastName, coursesTaken, rating);
+        this.email = email;
+    }
+
+    public SpecialStudent(String lastName, Course[] coursesTaken, long id, String email) {
+        super(lastName, coursesTaken, id);
+        this.email = email;
+    }
 }
