1b5497f90dc1ea1c024e4ac20fe08c56644439e1
Dmitriy <saltykovdim@gmail.com>
2016-09-06 17:16:05 +0300

    модуль 4 зад4

---
+++ src/module_4/task_1/User.java
@@ -0,0 +1,25 @@
+package module_4.task_1;
+
+
+public class User {
+    private long id;
+    private String name;
+    private double balance;
+    private int monthsOfEmployment;
+    private String companyName;
+    private int salary;
+    private Bank bank;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", balance=" + balance +
+                ", monthsOfEmployment=" + monthsOfEmployment +
+                ", companyName='" + companyName + '\'' +
+                ", salary=" + salary +
+                ", bank=" + bank +
+                '}';
+    }
+}
