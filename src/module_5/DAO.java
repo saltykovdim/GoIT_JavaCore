b2b5e53054a5b34cab3e54b43d358aa358db7cd1
Dmitriy <saltykovdim@gmail.com>
2016-09-14 20:44:42 +0300

    модуль 5 зад4

--- src/module_5/DAO.java
+++ src/module_5/DAO.java
@@ -3,7 +3,10 @@ package module_5;
 
 public interface DAO {
     Room save(Room room);
+
     boolean delete(Room room);
+
     Room update(Room room);
+
     Room findById(long id);
 }
--- src/module_5/TripAdvisorAPI.java
+++ src/module_5/TripAdvisorAPI.java
@@ -3,21 +3,22 @@ package module_5;
 
 import java.util.Date;
 
-public class TripAdvisorAPI implements API{
+public class TripAdvisorAPI implements API {
     Room[] rooms = new Room[5];
+
     @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
         Date someDate = new Date();
-        Room room1 = new Room(21, 625, 2, someDate ,"Sputnic", "Dnepr");
-        rooms[0]=room1;
-        Room room2 = new Room(22, 550, 2,someDate ,"Sputnic", "Dnepr");
-        rooms[1]=room2;
-        Room room3= new Room(23, 650, 1,someDate, "Sputnic", "Dnepr");
-        rooms[2]= room3;
-        Room room4=new Room(24, 500, 1, someDate,"Sputnic", "Dnepr");
-        rooms[3]=room4;
-        Room room5= new Room(25, 800, 4,someDate, "Sputnic", "Dnepr");
-        rooms[4]=room5;
+        Room room1 = new Room(21, 625, 2, someDate, "Sputnic", "Dnepr");
+        rooms[0] = room1;
+        Room room2 = new Room(22, 550, 2, someDate, "Sputnic", "Dnepr");
+        rooms[1] = room2;
+        Room room3 = new Room(23, 650, 1, someDate, "Sputnic", "Dnepr");
+        rooms[2] = room3;
+        Room room4 = new Room(24, 500, 1, someDate, "Sputnic", "Dnepr");
+        rooms[3] = room4;
+        Room room5 = new Room(25, 800, 4, someDate, "Sputnic", "Dnepr");
+        rooms[4] = room5;
         return rooms;
     }
 }
