5dd961e96b1008577d6462c5576e8952343c069f
Dmitriy <saltykovdim@gmail.com>
2016-09-16 13:52:25 +0300

    модуль 5 зад5

--- src/module_5/DAOImpl.java
+++ src/module_5/DAOImpl.java
@@ -2,30 +2,30 @@ package module_5;
 
 import java.util.Date;
 
-public class DAOImpl implements DAO{
+public class DAOImpl implements DAO {
     @Override
     public Room save(Room room) {
-        System.out.println("save "+room);
+        System.out.println("save " + room);
         return room;
     }
 
     @Override
     public boolean delete(Room room) {
-        System.out.println("your room "+room+" delete");
+        System.out.println("your room " + room + " delete");
         return true;
     }
 
     @Override
     public Room update(Room room) {
-        System.out.println("your room "+room+ " update");
+        System.out.println("your room " + room + " update");
         return room;
     }
 
     @Override
     public Room findById(long id) {
         Date someDate = new Date();
-        Room room = new Room(id, 0, 0,someDate,null, null);
-        System.out.println("your room with "+id+" found");
+        Room room = new Room(id, 0, 0, someDate, null, null);
+        System.out.println("your room with " + id + " found");
         return room;
     }
 }
