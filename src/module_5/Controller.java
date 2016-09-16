90b8de260cfb90268872cdaa70872713ba523abd
Dmitriy <saltykovdim@gmail.com>
2016-09-16 16:18:37 +0300

    модуль 5 зад6

--- src/module_5/Controller.java
+++ src/module_5/Controller.java
@@ -15,20 +15,20 @@ public class Controller {
         Room[] room1 = apis[0].findRooms(price, persons, city, hotel);
         Room[] room2 = apis[1].findRooms(price, persons, city, hotel);
         Room[] room3 = apis[2].findRooms(price, persons, city, hotel);
-        count=room1.length + room2.length + room3.length;
+        count = room1.length + room2.length + room3.length;
         Room[] rooms = new Room[count];
-        for(int i=0; i<rooms.length;i++){
+        for (int i = 0; i < rooms.length; i++) {
             for (int room1i = 0; i < room1.length; i++) {
-                rooms[i]=room1[room1i];
+                rooms[i] = room1[room1i];
             }
             for (int room2i = 0; i < room2.length; i++) {
-                rooms[i]=room2[room2i];
+                rooms[i] = room2[room2i];
             }
             for (int room3i = 0; i < room3.length; i++) {
-                rooms[i]=room3[room3i];
+                rooms[i] = room3[room3i];
             }
         }
-       return rooms;
+        return rooms;
     }
 
 
