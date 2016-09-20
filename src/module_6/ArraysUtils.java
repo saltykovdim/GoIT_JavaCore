5e597dd51bc2d9e779e05c7c1031f8c623551214
Dmitriy <saltykovdim@gmail.com>
2016-09-20 12:01:49 +0300

    модуль 6 зад1

---
+++ src/module_6/ArraysUtils.java
@@ -0,0 +1,39 @@
+package module_6;
+
+public class ArraysUtils {
+    private int[] sum(int array[]) {
+        return null;
+    }
+
+    private int[] min(int array[]) {
+        return null;
+    }
+
+    private int[] max(int array[]) {
+        return null;
+    }
+
+    private int[] maxPositive(int array[]) {
+        return null;
+    }
+
+    private int[] multiplication(int array[]) {
+        return null;
+    }
+
+    private int[] modulus(int array[]){
+        return null;
+    }
+
+    private int[] secondLargest(int array[]){
+        return null;
+    }
+
+    private int[] reverse(int[] array){
+        return null;
+    }
+
+    private int[] findEvenElements(int[] array){
+        return null;
+    }
+}
