e2d85879cf939a0047e4a484f7808d9f0b403802
Dmitriy <saltykovdim@gmail.com>
2016-09-23 13:58:06 +0300

    модуль 6 зад2,5,6

--- src/module_6/User.java
+++ src/module_6/User.java
@@ -40,6 +40,37 @@ public class User {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        if (getId() == user.getId() && getLastName().equals(user.getLastName())) {
+            if (getFirstName().equals(user.getFirstName()) && getSalary() == user.getSalary()) {
+                if (getBalance() == user.getBalance()) return true;
+            }
+        }
+        if (getId() != user.getId()) return false;
+        if (getSalary() != user.getSalary()) return false;
+        if (getBalance() != user.getBalance()) return false;
+        if (getFirstName() != null ? !getFirstName().equals(user.getFirstName()) : user.getFirstName() != null)
+            return false;
+        return getLastName() != null ? getLastName().equals(user.getLastName()) : user.getLastName() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (getId() ^ (getId() >>> 32));
+        result = 31 * result + (getFirstName() != null ? getFirstName().hashCode() : 0);
+        result = 31 * result + (getLastName() != null ? getLastName().hashCode() : 0);
+        result = 31 * result + getSalary();
+        result = 31 * result + getBalance();
+        return result;
+    }
+
+    @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
