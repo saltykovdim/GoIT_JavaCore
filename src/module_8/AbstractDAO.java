11380b8ccb7f4690c6ab7179add1d43e05ad1616
Dmitriy <saltykovdim@gmail.com>
2016-10-10 17:24:35 +0300

    mod 8 task 2

--- src/module_8/AbstractDAO.java
+++ src/module_8/AbstractDAO.java
@@ -4,9 +4,17 @@ package module_8;
 import java.util.List;
 
 public interface AbstractDAO<T> {
-   public List<T> save(List<T> list);
+    public List<T> save(List<T> list);
+
     public void delete(List<T> list);
+
     public void deleteAll(List<T> list);
+
     public void saveAll(List<T> list);
-    public List <T> getList();
+
+    public List<T> getList();
+
+    public void deleteById(long id);
+
+    public List<T> get(long id);
 }
