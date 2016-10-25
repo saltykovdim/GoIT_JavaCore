3cb78555c009722950f96f1afee3b86870249f5b
Dmitriy <saltykovdim@gmail.com>
2016-10-25 11:31:30 +0300

    mod 10 task 2

--- src/module_10/Main.java
+++ src/module_10/Main.java
@@ -2,14 +2,30 @@ package module_10;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MyException {
+        //task 1
+        System.out.println("TASK 1.....");
         try {
             int a = 0;
-            System.out.println(10/a);
-        }catch (ArithmeticException e){
-            System.out.println("You get an Arithmetic Exception.");
-        }finally {
+            System.out.println(10 / a);
+        } catch (ArithmeticException e) {
+            System.out.println("You got an Arithmetic Exception.");
+        } finally {
             System.out.println("Finally block is made.");
+        }
+
+        //task 2
+        System.out.println();
+        System.out.println("TASK 2....");
+        try {
+            String s= "hello";
+            if (s.equals("hello")) {
+                throw new MyException(s);
+            }else{
+                System.out.println("You not got your own Exception");
+            }
+        } catch (MyException e) {
+            System.out.println("You got your own Exception.");
         }
     }
 }
---
+++ src/module_10/MyException.java
@@ -0,0 +1,8 @@
+package module_10;
+
+
+public class MyException extends Exception{
+    public MyException(String s){
+        super(s);
+    }
+}
